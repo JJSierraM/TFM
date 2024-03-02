219a064bc3cfd0aeb226d64a0cd1d1d25303114b
NunoSempere <nuno.sempere@protonmail.com>
2024-03-02 15:42:17 -0300

    add some debug statements to see what's up with invalid read

--- include/Debug.h
+++ include/Debug.h
@@ -4,6 +4,6 @@
 #define DEBUG
 
 #define VERBOSE 1
-#define DEBUG_PRINTLN(...) if(VERBOSE){ printf(##__VA_ARGS__); printf(" @ %s:%d\n", __FILE__, __LINE__) }
+#define DEBUG_PRINTLN(...) if(VERBOSE) printf(__VA_ARGS__); printf(" @ %s:%d\n", __FILE__, __LINE__); 
 
 #endif
